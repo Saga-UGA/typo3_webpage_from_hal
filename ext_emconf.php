659b0db011cc2d5e88e7d3f811d19bd4a867d99b
Boris Morel <Boris.Morel@imag.fr>
2014-05-12 10:33:41 +0200

    Start plugin

---
+++ ext_emconf.php
@@ -0,0 +1,13 @@
+<?php
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'Saga',
+    'description' => 'Tools to export your publication list from Hal',
+    'category' => 'plugin',
+    'author' => 'Morel Boris',
+    'author_company' => 'SAGA',
+    'author_email' => 'boris.morel@imag.fr',
+    'dependencies' => 'extbase,fluid',
+    'state' => 'beta',
+    'clearCacheOnLoad' => '1',
+    'version' => '1.0',
+);
